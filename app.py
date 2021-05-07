9325de1a284588ae53b60edc9b896e4b6e114fe1
yaswanth118 <yaswanthchunduru118@gmail.com>
2021-05-07 22:46:48 +0530

    ninth commit

--- app.py
+++ app.py
@@ -498,8 +498,8 @@ def delete_article(id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.secret_key = 'secret123'
     app.run()
+app.secret_key = 'secret123'
 
 
 
