6aa0256ff8090ef4385af29ecf9e4b3acdb529f5
yaswanth118 <yaswanthchunduru118@gmail.com>
2021-05-11 10:13:38 +0530

    twelth commit

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from functools import wraps
 
 app = Flask(__name__)
 
-# app.config['MYSQL_HOST'] = 'remotemysql.com'
-# app.config['MYSQL_USER'] = 'nU0amtc0De'
-# app.config['MYSQL_PASSWORD'] = 'OOP4GV0SII'
-# app.config['MYSQL_DB'] = 'nU0amtc0De'
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'yaswanth2001'
-app.config['MYSQL_DB'] = 'virtuallab'
+app.config['MYSQL_HOST'] = 'remotemysql.com'
+app.config['MYSQL_USER'] = 'nU0amtc0De'
+app.config['MYSQL_PASSWORD'] = 'OOP4GV0SII'
+app.config['MYSQL_DB'] = 'nU0amtc0De'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'yaswanth2001'
+# app.config['MYSQL_DB'] = 'virtuallab'
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 #--------------------------------------------------------------------#
@@ -498,8 +498,8 @@ def delete_article(id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.secret_key = 'secret123'
     app.run()
+app.secret_key = 'secret123'
 
 
 
